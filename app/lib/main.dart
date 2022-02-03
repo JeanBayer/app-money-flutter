f34037787e06e7f34d75a82af35a99c998b70093
Jean Bayer <jhanbayer@gmail.com>
2022-02-03 13:05:14 -0300

    modal bottom sheet add

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,42 +1,98 @@
 import 'package:flutter/material.dart';
-import './widgets/user_transaction.dart';
+import './models/transaction.dart';
+import './widgets/new_transaction.dart';
+import './widgets/transaction_list.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter App',
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransaction = [
+    Transaction(
+      id: "t1",
+      title: "food",
+      amount: 13.25,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: "shoes",
+      amount: 3.25,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _newUserTransaction(String title, double amount) {
+    final newTx = Transaction(
+      id: DateTime.now().toString(),
+      title: title,
+      amount: amount,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _userTransaction.add(newTx);
+    });
+  }
+
+  void _addNewTx(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return NewTransaction(_newUserTransaction);
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("prueba"),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blueGrey,
-                child: Text("CHART"),
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              icon: Icon(
+                Icons.add,
               ),
+              onPressed: () => _addNewTx(context),
             ),
-            UserTransaction(),
           ],
+          title: Text("prueba"),
         ),
-      ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                child: Card(
+                  color: Colors.blueGrey,
+                  child: Text("CHART"),
+                ),
+              ),
+              TransactionList(_userTransaction),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => _addNewTx(context),
+          child: Icon(
+            Icons.add,
+          ),
+        ),
     );
   }
 }
